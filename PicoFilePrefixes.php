dada09caa37e056149e215e1ba8ea6a7f3501abe
Daniel Rudolf <github.com@daniel-rudolf.de>
2016-09-17 20:54:19 +0200

    Version 1.0.2
    ```
    * [New] Add `CHANGELOG.md`
    * [Changed] Rename composer package to `phrozenbyte/pico-file-prefixes` and
                publish it on Packagist
    ```

--- PicoFilePrefixes.php
+++ PicoFilePrefixes.php
@@ -11,7 +11,7 @@
  * @author  Daniel Rudolf
  * @link    http://picocms.org
  * @license http://opensource.org/licenses/MIT The MIT License
- * @version 1.0.1
+ * @version 1.0.2
  */
 class PicoFilePrefixes extends AbstractPicoPlugin
 {
